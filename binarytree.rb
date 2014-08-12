849e1effea6e2e4e3d1f4e655ba3a49574abd78b
Michael Alexander <alexandermw@gmail.com>
2014-08-12 12:43:09 -0700

    created first pass at Tree class

--- binarytree.rb
+++ binarytree.rb
@@ -1,4 +1,39 @@
-module BinaryTree
+class Tree
+  attr_reader :root_node
+  
+  def initialize(values)
+    @root_node = nil
+    build_tree(values)
+  end
+
+  
+  def build_tree(values)
+    values.shuffle!
+    values.each do |value|
+      add_node(value)
+    end
+  end
+  
+  def add_node(current_node = nil, value)
+    if @root_node == nil 
+      @root_node = Node.new(value)
+    elsif value <= current_node.value
+      if current_node.left_child == nil
+        current_node.left_child = Node.new(value, {parent: current_node})
+      else
+        add_node(current_node.left_child, value)
+      end
+    elsif value >= current_node.value
+      if current_node.right_child == nil
+        current_node.right_child = Node.new(value, {parent: current_node})
+      else
+        add_node(current_node.right_child, value)
+      end
+    end
+    
+  end
+  
+end
 
 class Node
   attr_accessor :value
@@ -24,16 +59,15 @@ class Node
     @right_child
   end
   
-  def set_left(new_child)
-    @left_child ||= new_child
+  def left_child=(new_child)
+    @left_child = new_child
   end
   
-  def set_right(new_child)
-    @right_child ||= new_child
+  def right_child=(new_child)
+    @right_child = new_child
   end
 
   private
   
 end
 
-end
