73c012269b5db7259b841c3b6e57af8fc9279fba
Michael Alexander <alexandermw@gmail.com>
2014-08-13 11:05:03 -0700

    Update knightstravails.rb

--- knightstravails.rb
+++ knightstravails.rb
@@ -1,17 +1,18 @@
 
-#Your task is to build a function knight_moves that shows the simplest possible way to get from one square to another by outputting all squares the knight will stop on along the way.
+#Your task is to build a function knight_moves that shows the simplest possible way to get from one square to
+#another by outputting all squares the knight will stop on along the way.
 
 #You can think of the board as having 2-dimensional coordinates.
 #Your function would therefore look like:
 #knight_moves([0,0],[1,2]) == [[0,0],[1,2]]
 #knight_moves([0,0],[3,3]) == [[0,0],[1,2],[3,3]]
 #knight_moves([3,3],[0,0]) == [[3,3],[1,2],[0,0]]
-#Put together a script that creates a game board and a knight.
-#Treat all the possible moves the knight could make as children in a tree.
+#1)Put together a script that creates a game board and a knight.
+#2)Treat all the possible moves the knight could make as children in a tree.
 #Don't allow any moves to go off the board.
-#Decide which search algorithm is best to use for this case.
+#3)Decide which search algorithm is best to use for this case.
 #Hint: one of them could be a potentially infinite series.
-#Use the chosen search algorithm to find the shortest path between the starting
+#4)Use the chosen search algorithm to find the shortest path between the starting
 # square (or node) and the ending square. Output what that full path looks like,
 # e.g.:
 #    > knight_moves([3,3],[4,3])
@@ -20,26 +21,60 @@
 #    [4,5]
 #    [2,4]
 #    [4,3]
-
-#takes position in form [0-7, 0-7]
-#returns an array of all legal moves for a knight from one position
-def possible_moves(position)
-  x = position[0]
-  y = position[1]
-  possibles = []
-  move_grid(x,y).each {|move| possibles << move if move_legal?(move)}
-  possibles
+Square = Struct.new(:position, :moves)
+class Square
+  def to_s
+    "position: #{position}, moves: #{moves}"
+  end
 end
 
-def move_grid(x, y)
-  [[x+2, y-1], [x+2, y+1], [x-2, y+1], [x-2, y-1],
-   [x+1, y+2], [x-1, y+2], [x+1, y-2], [x-1, y-2]]
+class MoveTree
+  attr_reader :start_node
+  
+  def initialize(start)
+    @start_node = Square.new(start, [])
+  end
+  
+  def next_moves(square)
+    possibles = possible_moves(square[:position])
+    possibles.each {|possible| square[:moves] << Square.new(possible, [])
+  end
+  
+  
+  private
+  
+  #takes position in form [0-7, 0-7]
+  #returns an array of all legal moves for a knight from one position
+  def possible_moves(position)
+    x, y = position[0], position[1]
+    possibles = []
+    
+    move_grid(x,y).each {|move| possibles << move if move_legal?(move)}
+    possibles
+  end
+    
+  def move_grid(x, y)
+    [[x+2, y-1], [x+2, y+1], [x-2, y+1], [x-2, y-1],
+     [x+1, y+2], [x-1, y+2], [x+1, y-2], [x-1, y-2]]
+  end
+
+  #checks if a move is legal for a knight from the current location
+  def move_legal?(move)
+    return true if move[0].between?(0, 7) && move[1].between?(0,7)
+    false
+  end
 end
 
-#checks if a move is legal for a knight from the current location
-def move_legal?(move)
-  return true if move[0].between?(0, 7) && move[1].between?(0,7)
-  false
-end
+include MoveTree
 
-p possible_moves([6,7])
+
+
+#def knight_moves(start, destination)
+
+#end
+  
+  
+  
+#script to test it
+t = MoveTree.new([6,7])
+p MoveTree.start_node
